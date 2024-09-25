9475ecb40baa32e62f9c2106f0c70c730afee510
samihalawa <samihalawaster@gmail.com>
2024-09-25 05:27:09 +0200

    Initial commit

---
+++ pollinations/types/__init__.py
@@ -0,0 +1,10 @@
+"""
+pollinations.types
+
+Classes:
+    ImageObject (types.ImageObject): Image object.
+    GitObject (types.GitObject): GitHub object.
+"""
+
+from .gitobject import GitObject
+from .imageobject import ImageObject
